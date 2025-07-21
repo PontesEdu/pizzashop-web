f4013315b8af0e47df9f0203eb4c6804f2f4ada6
PontesEdu <edupontessilva03@gmail.com>
2025-07-20 22:23:44 -0300

    criei layouts app auth o header e navlink com components shadcn e add thema dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="system">
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
+    </>
+  )
 }
---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,104 @@
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import z from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const signUpForm = z.object({
+  restaurantName: z.string(),
+  managerName: z.string(),
+  phone: z.string(),
+  email: z.email(),
+})
+
+type SignUpForm = z.infer<typeof signUpForm>
+
+export function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignUpForm>()
+
+  const navigate = useNavigate()
+
+  async function handleSignUp(data: SignUpForm) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Cadastro criado com sucesso!.', {
+        action: {
+          label: 'Login',
+          onClick: () => navigate('/sign-in'),
+        },
+      })
+    } catch {
+      toast.error('Credenciais inválidas.')
+    }
+  }
+
+  return (
+    <div className="p-8">
+      <Button variant="ghost" asChild className="absolute top-8 right-8">
+        <Link to="/sign-in">Novo estabelecimento</Link>
+      </Button>
+
+      <div className="flex w-[320px] flex-col justify-center gap-6">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Criar conta grátis
+          </h1>
+
+          <p className="text-muted-foreground text-sm">
+            Seja um parceiro e começe suas vendas!
+          </p>
+        </div>
+
+        <form className="space-y-4" onSubmit={handleSubmit(handleSignUp)}>
+          <div className="space-y-2">
+            <Label htmlFor="restaurant">Nome do estabelecimento</Label>
+            <Input
+              id="restaurant"
+              type="text"
+              {...register('restaurantName')}
+            />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="name">Seu nome</Label>
+            <Input id="name" type="text" {...register('managerName')} />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-mail</Label>
+            <Input id="email" type="email" {...register('email')} />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="phone">phone</Label>
+            <Input id="phone" type="tel" {...register('phone')} />
+          </div>
+
+          <Button disabled={isSubmitting} className="w-full" type="submit">
+            Finalizar cadastro
+          </Button>
+
+          <p className="text-muted-foreground px-6 text-center text-sm leading-relaxed">
+            ao continuar você concorda com nosso{' '}
+            <a href="#" className="underline underline-offset-4">
+              Termo de serviço
+            </a>{' '}
+            e{' '}
+            <a href="" className="underline underline-offset-4">
+              politicas de privacidade
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
+  )
+}
